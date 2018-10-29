e7faf498ae191e1d798ecab03d82244ad52648f1
midephysco <44247592+midephysco@users.noreply.github.com>
2018-10-29 22:07:24 +0100

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,8 @@
+def main():
+     print("what is your name? ")
+     yourName = input()
+     print("Hello{0} ". format(yourName))
+     #what is your age
+     yourAge = int(input("what is your age ? "))
+     print("Hello {0}. you are a young {1}" .format(yourName,yourAge))
+main()
